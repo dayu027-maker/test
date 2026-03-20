5efe16570308660a4fb688b63c01a3e6608b1985
gaitianyu <dayu027@163.com>
2026-03-20 11:21:53 +0800

    删除无用文件

--- test_st_main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-try:
-    from streamlit.runtime.scriptrunner import get_script_run_ctx
-except ImportError:
-    from streamlit.scriptrunner import get_script_run_ctx
-
-print(f"CTX: {get_script_run_ctx()}")
-if __name__ == "__main__":
-    pass
